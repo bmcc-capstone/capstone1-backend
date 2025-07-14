e6eced4f0716e64653428ac6f5337b6dd3ddc949
jeramyleon <jeramyleon@gmail.com>
2025-07-14 14:41:30 -0400

    rename file from ballet to ballot.js

---
+++ database/Ballot.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const db = require("./db");
+
+const Ballot = db.define("ballot", {
+  ballot_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  poll_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Ballot;
