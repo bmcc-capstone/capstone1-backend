57fdb87ab68e599619f205081235e90858c11f05
jeramyleon <jeramyleon@gmail.com>
2025-07-14 13:43:50 -0400

    ballot model defined

---
+++ database/Ballet.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const db = require("./db");
+
+const Ballot = db.define("ballot", {
+  ballot_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  poll_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+  },
+  user_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Ballot;
