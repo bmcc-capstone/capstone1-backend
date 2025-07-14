e7ec301c96d105d87bc4f7a469382dfb897e0aeb
Guarotav <guarotav@gmail.com>
2025-07-14 13:15:59 -0400

    Changes to names in index

--- database/index.js
+++ database/index.js
@@ -3,11 +3,11 @@ const db = require("./db");
 
 const User = require("./user")(db, Sequelize.DataTypes);
 const Poll = require("./Poll")(db, Sequelize.DataTypes);
-const Option = require("./Option")(db, Sequelize.DataTypes);
-const Vote = require("./Vote")(db, Sequelize.DataTypes);
+const Option = require("./pollOptions")(db, Sequelize.DataTypes);
+const Vote = require("./Ballot")(db, Sequelize.DataTypes);
 const Rank = require("./Rank")(db, Sequelize.DataTypes);
-const Ballet = require("./Ballet")(db, Sequelize.DataTypes);
-const BalletItems = require("./BalletItems")(db, Sequelize.DataTypes);
+const Ballot = require("./Ballet")(db, Sequelize.DataTypes);
+const BallotItems = require("./BalletItems")(db, Sequelize.DataTypes);
 
 User.hasMany(Poll, { foreignKey: "userId" });
 Poll.belongsTo(User, { foreignKey: "userId" });
@@ -16,13 +16,13 @@ Poll.hasMany(Option, { foreignKey: "pollId", onDelete: "CASCADE" });
 Option.belongsTo(Poll, { foreignKey: "pollId" });
 
 Poll.hasMany(Vote, { foreignKey: "pollId", onDelete: "CASCADE" });
-Vote.belongsTo(Poll, { foreignKey: "pollId" });
+Ballot.belongsTo(Poll, { foreignKey: "pollId" });
 
 User.hasMany(Vote, { foreignKey: "userId" });
-Vote.belongsTo(User, { foreignKey: "userId" });
+Ballot.belongsTo(User, { foreignKey: "userId" });
 
 Rank.hasMany(Option, { foreignKey: "rankId" });
 Rank.belongsTo(Option, { foreignKey: "optionId" });
 
-Ballet.hasMany(BalletItems, { foreignKey: "balletId", onDelete: "CASCADE" });
-BalletItems.belongsTo(Ballet, { foreignKey: "balletId" });
+Ballot.hasMany(BalletItems, { foreignKey: "ballotId", onDelete: "CASCADE" });
+BallotItems.belongsTo(Ballot, { foreignKey: "ballotId" });
