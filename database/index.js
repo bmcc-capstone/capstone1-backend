bb68255e91e56adf90d4f03b4f1ded34220daac6
JoseG2704 <joseg50274@gmail.com>
2025-07-14 11:58:22 -0400

    Updated Index and added BalletItems as a partial placeholder

--- database/index.js
+++ database/index.js
@@ -6,6 +6,8 @@ const Poll = require("./Poll")(db, Sequelize.DataTypes);
 const Option = require("./Option")(db, Sequelize.DataTypes);
 const Vote = require("./Vote")(db, Sequelize.DataTypes);
 const Rank = require("./Rank")(db, Sequelize.DataTypes);  
+const Ballet = require("./Ballet")(db, Sequelize.DataTypes);
+const BalletItems = require("./BalletItems")(db, Sequelize.DataTypes);
 
 User.hasMany(Poll, { foreignKey: "userId" });
 Poll.belongsTo(User, { foreignKey: "userId" });
@@ -21,4 +23,7 @@ Vote.belongsTo(User, { foreignKey: "userId" });
 
 Rank.hasMany(Option, { foreignKey: "rankId" });
 Rank.belongsTo(Option, { foreignKey: "optionId" });
+
+Ballet.hasMany(BalletItems, { foreignKey: "balletId", onDelete: "CASCADE" });
+BalletItems.belongsTo(Ballet, { foreignKey: "balletId" });
 
