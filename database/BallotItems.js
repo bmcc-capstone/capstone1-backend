2b3d98b12315da3b56e0aa6cc7a9ae8e8de5525e
JoseG2704 <joseg50274@gmail.com>
2025-07-14 14:29:50 -0400

    Added rank into BallotItems and fixed index

---
+++ database/BallotItems.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, DataTypes) => {
+    const BallotItem = sequelize.define("BallotItem", {
+      rank_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+  
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "User",
+          key: "user_id",
+        },
+      },
+  
+      ballot_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Ballot",
+          key: "ballot_id",
+        },
+      },
+  
+      option_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "pollOption",
+          key: "option_id",
+        },
+      },
+  
+      poll_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "pollTable",
+          key: "poll_id",
+        },
+      },
+  
+      rank: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    });
+  
+    return BallotItem;
+  };
+  
