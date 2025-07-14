d41a8cab6827f87dea3475945101656174fba9c6
Guarotav <guarotav@gmail.com>
2025-07-14 11:38:40 -0400

    DataTypes model defined

--- database/user.js
+++ database/user.js
@@ -34,7 +34,8 @@ const User = db.define("user", {
   profilePicture: {
     type: DataTypes.STRING,
     allowNull: true,
-    defaultValue: "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg",
+    defaultValue:
+      "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg",
   },
 
   auth0Id: {
