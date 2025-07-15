4207a4132da9749e611c0c49c66e833763d18bf5
Benjamin Ayala <Benjaminayala607@gmail.com>
2025-07-15 15:09:51 -0400

    Updated poll model, added delete poll options, cleaned up routes

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const testDbRouter = require("./test-db");
-const pollRouter = require("./Poll"); // 
+const pollRouter = require("./Poll"); //
+const pollOptionRouter = require("./pollOptions");
+const ballotRouter = require("./Ballot");
 
+router.use("/ballots", ballotRouter);
+router.use("/poll-options", pollOptionRouter); // 
 router.use("/test-db", testDbRouter);
-router.use("/polls", pollRouter);     // 
+router.use("/polls", pollRouter); //
 
 module.exports = router;
