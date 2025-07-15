faa13beead4dc03d3f2ae43a1094832930ab30c3
jeramyleon <jeramyleon@gmail.com>
2025-07-15 11:52:16 -0400

    .

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 const db = require("./db");
 
-const User = require("./user")(db, Sequelize.DataTypes);
-const Poll = require("./pollTable")(db, Sequelize.DataTypes);
-const PollOption = require("./pollOptions")(db, Sequelize.DataTypes);
-const Ballot = require("./Ballot")(db, Sequelize.DataTypes);
-const BallotItem = require("./BallotItems")(db, Sequelize.DataTypes);
+const User = require("./user") (db, Sequelize.DataTypes);
+const Poll = require("./pollTable") (db, Sequelize.DataTypes);
+const PollOption = require("./pollOptions") (db, Sequelize.DataTypes);
+const Ballot = require("./Ballot") (db, Sequelize.DataTypes);
+const BallotItem = require("./BallotItems") (db, Sequelize.DataTypes);
 
 User.hasMany(Poll, { foreignKey: "user_id" });
 Poll.belongsTo(User, { foreignKey: "user_id" });
