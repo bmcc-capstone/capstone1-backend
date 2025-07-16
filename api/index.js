c6f571c3eb6d2f78123f8ef958e49fb9839dab89
jeramyleon <jeramyleon@gmail.com>
2025-07-16 10:54:06 -0400

    can now get all ballots by specific poll ID

--- api/index.js
+++ api/index.js
@@ -7,9 +7,8 @@ const pollOptionRouter = require("./pollOptions");
 const ballotRouter = require("./Ballot");
 
 router.use("/ballots", ballotRouter);
-router.use("/poll-options", pollOptionRouter); // 
+router.use("/poll-options", pollOptionRouter); //
 router.use("/test-db", testDbRouter);
-router.use("/deletePollOption", deletePollOptionRoutes);
 router.use("/polls", pollRouter); //
 
 module.exports = router;
